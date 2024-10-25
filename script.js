16185c5781b3bbc65d94796fb8d28cbe0b07bfac
Mridula Chowdhary <mridulachowdhary21@gmail.com>
2024-10-25 16:50:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+async function fetchAdvice() {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const data = await response.json();
+      document.querySelector('.advice-id').textContent = `Advice #${data.slip.id}`;
+      document.querySelector('.advice-text').textContent = `"${data.slip.advice}"`;
+    } catch (error) {
+      console.error('Error fetching advice:', error);
+    }
+  }
+  
